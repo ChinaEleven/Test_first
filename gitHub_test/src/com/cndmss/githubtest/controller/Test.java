047077c5e72c4f4a996e8425ee27e65366b54c98
ChinaEleven <cndmss@163.com>
2015-07-27 18:05:39 +0800

    changes log

---
+++ gitHub_test/src/com/cndmss/githubtest/controller/Test.java
@@ -0,0 +1,7 @@
+package com.cndmss.githubtest.controller;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello word~");
+	}
+}
---
+++ gitHub_test/src/com/cndmss/githubtest/util/DateUtil.java
@@ -0,0 +1,5 @@
+package com.cndmss.githubtest.util;
+
+public class DateUtil {
+
+}
